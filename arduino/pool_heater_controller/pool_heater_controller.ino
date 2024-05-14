369076c257a862de608578eb6df3f87699c5f5db
tobi-repos <tobias.neudeck@gmail.com>
2024-05-14 20:47:53 +0200

    Added flag to disable radio module.

--- arduino/pool_heater_controller/pool_heater_controller.ino
+++ arduino/pool_heater_controller/pool_heater_controller.ino
@@ -1,5 +1,7 @@
 #include <RF24.h>
 
+#define WITH_RADIO 0
+
 static const int POOL_PUMP_IN = 2;
 static const int SONOFF_IN = 3;
 static const int SONOFF_OUT = 4;
@@ -7,6 +9,8 @@ static const int POOL_PUMP_OUT = 5;
 static const int HEATING_SYS_OUT = 6;
 static const int HEATING_SYS_ERR_OUT = 7;
 static const int HEATING_VALVE_OUT = 8;
+
+#if WITH_RADIO
 static const int RF_CE = 9;
 static const int RF_CSN = 10;
 
@@ -15,6 +19,7 @@ static RF24 radio(RF_CE, RF_CSN);
 static const uint8_t radioId[5] = "POOL";
 static uint8_t radioPayload[1] = {0};
 static uint32_t lastRadioMessage = 0;
+#endif
 
 static bool poolPumpOk = false;
 static bool sonoffOk = false;
@@ -32,13 +37,17 @@ void setup()
 	pinMode(HEATING_SYS_ERR_OUT, OUTPUT);
 	pinMode(HEATING_VALVE_OUT, OUTPUT);
 
+#if WITH_RADIO
 	radio.begin();
 	radio.setChannel(1);
 	radio.setPALevel(RF24_PA_LOW);
 	radio.setAddressWidth(4);
 	radio.setPayloadSize(sizeof(radioPayload));
 	radio.openReadingPipe(0, radioId);
-	radio.startListening(); 
+	radio.startListening();
+#else
+    heatingSysOk = true;
+#endif
 }
 
 void loop()
@@ -67,6 +76,7 @@ void loop()
 		sonoffOk = false;
 	}
 	
+#if WITH_RADIO
 	if (radio.available())
 	{
 		radio.read(radioPayload, sizeof(radioPayload));
@@ -91,6 +101,10 @@ void loop()
 		digitalWrite(HEATING_SYS_ERR_OUT, HIGH);
 		heatingSysOk = false;
 	}
+#else
+    digitalWrite(HEATING_SYS_ERR_OUT, LOW);
+    digitalWrite(HEATING_SYS_OUT, HIGH);
+#endif
 
 	if (conditionsFulfilled)
 	{
